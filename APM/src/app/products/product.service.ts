d025e7cd19a06968b22fc9c79b13a94a6cd12766
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-03-04 10:32:48 -0300

    FEAT map products price

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 import { Product } from './product';
 
@@ -9,10 +9,15 @@ import { Product } from './product';
   providedIn: 'root',
 })
 export class ProductService {
-  private productsUrl = 'api/product';
+  private productsUrl = 'api/products';
   private suppliersUrl = 'api/suppliers';
 
   products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    map(products => products.map(products => ({
+      ...products,
+      price: products.price? products.price * 1.5 : 0,
+      searchKey: [products.productName]
+    }as Product))),
     tap((data) => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
